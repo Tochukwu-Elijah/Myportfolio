4615bbf2a58826d030d9d9916546239caa26b868
Eze Tochukwu <157438236+Tochukwu-Elijah@users.noreply.github.com>
2024-04-01 17:40:12 +0100

    Update Testimonial.tsx

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -116,7 +116,20 @@ const Testimonial = () => {
                     </p> */}
                   </div>
                 </div>
-                {/* <div className="w-full lgl:w-[60%] h-full flex flex-col justify-between">
+
+              </div>
+            </div>
+            {/* ================ Slider Two ================== */}
+
+            <div className="w-full">
+              <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
+                <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
+                  <img
+                    className="h-72 md:h-32 lgl:h-72 rounded-lg object-cover"
+                    src={testimonialfive}
+                    alt="testimonialOne"
+                  />
+                                  {/* <div className="w-full lgl:w-[60%] h-full flex flex-col justify-between">
                   <img className="w-20 lgl:w-32" src={quote} alt="quote" />
                   <div className="w-full h-[70%] py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
                     <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
@@ -145,18 +158,6 @@ const Testimonial = () => {
                     </p>
                   </div>
                 </div> */}
-              </div>
-            </div>
-            {/* ================ Slider Two ================== */}
-
-            <div className="w-full">
-              <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
-                <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
-                  <img
-                    className="h-72 md:h-32 lgl:h-72 rounded-lg object-cover"
-                    src={testimonialfive}
-                    alt="testimonialOne"
-                  />
                   <div className="w-full flex flex-col justify-end">
                     <p className="text-xs uppercase text-designColor tracking-wide mb-2">
                       Available On Request
